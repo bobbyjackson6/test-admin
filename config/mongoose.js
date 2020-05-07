9b934158f418001ed1851c99aefdfda86bf8bc81
Vladislav Uzelman <36370246+bobbyjackson6@users.noreply.github.com>
2020-05-07 21:02:56 +0300

    Отделил файл БД от общего кода.

---
+++ config/mongoose.js
@@ -0,0 +1,181 @@
+const mongoose = require("mongoose");
+// Mongoose
+
+mongoose
+  .connect(
+    "mongodb+srv://redalert:redalert@cluster0-vzasu.mongodb.net/promo-csmoney?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+mongoose.set("debug", true);
+const AdminSchema = new mongoose.Schema({
+  login: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// pagesSchema
+
+const PageSchema = new mongoose.Schema({
+  title_en: {
+    $regex: /\w/,
+    type: String,
+  },
+  description_en: {
+    type: String,
+  },
+  alias_en: {
+    type: String,
+  },
+  headerLogo: {
+    type: String,
+  },
+  chatLogo: {
+    type: String,
+  },
+  bloggerName_en: {
+    type: String,
+  },
+  mainHeader_en: {
+    type: String,
+  },
+  underTitleText_en: {
+    type: String,
+  },
+  buttonText_en: {
+    type: String,
+  },
+  infoHeader_en: {
+    type: String,
+  },
+  firstImageText_en: {
+    type: String,
+  },
+  secondImageText_en: {
+    type: String,
+  },
+  thirdImageText_en: {
+    type: String,
+  },
+  fourthImageText_en: {
+    type: String,
+  },
+  feedbackHeader_en: {
+    type: String,
+  },
+  feedbackText_en: {
+    type: String,
+  },
+  reviewLink_en: {
+    type: String,
+  },
+  csmoneyAnswer_en: {
+    type: String,
+  },
+  firstVideoLink: {
+    type: String,
+  },
+  secondVideoLink: {
+    type: String,
+  },
+  thirdVideoLink: {
+    type: String,
+  },
+  useCsmoneyTitle_en: {
+    type: String,
+  },
+  finalBlockHeader_en: {
+    type: String,
+  },
+  finalBlockText_en: {
+    type: String,
+  },
+  promocode: {
+    type: String,
+  },
+  finalButtonText_en: {
+    type: String,
+  },
+  textUnderFinalButton_en: {
+    type: String,
+  },
+
+  title_ru: {
+    $regex: /\w/,
+    type: String,
+  },
+  description_ru: {
+    type: String,
+  },
+  alias_ru: {
+    type: String,
+  },
+  bloggerName_ru: {
+    type: String,
+  },
+  mainHeader_ru: {
+    type: String,
+  },
+  underTitleText_ru: {
+    type: String,
+  },
+  buttonText_ru: {
+    type: String,
+  },
+  infoHeader_ru: {
+    type: String,
+  },
+  firstImageText_ru: {
+    type: String,
+  },
+  secondImageText_ru: {
+    type: String,
+  },
+  thirdImageText_ru: {
+    type: String,
+  },
+  fourthImageText_ru: {
+    type: String,
+  },
+  feedbackHeader_ru: {
+    type: String,
+  },
+  feedbackText_ru: {
+    type: String,
+  },
+  reviewLink_ru: {
+    type: String,
+  },
+  csmoneyAnswer_ru: {
+    type: String,
+  },
+  useCsmoneyTitle_ru: {
+    type: String,
+  },
+  finalBlockHeader_ru: {
+    type: String,
+  },
+  finalBlockText_ru: {
+    type: String,
+  },
+  finalButtonText_ru: {
+    type: String,
+  },
+  textUnderFinalButton_ru: {
+    type: String,
+  },
+});
+
+const Admins = mongoose.model("admins", AdminSchema);
+const Page = mongoose.model("pages", PageSchema);
+// End mongo
+
+module.exports.Admins = Admins;
+module.exports.Page = Page;
--- main.js
+++ main.js
@@ -1,128 +1,22 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const { createServer } = require("http");
-
 const app = express();
-const port = 3000;
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.set("view engine", "ejs");
+const http = require("http").createServer(app);
 
+const Page = require("./config/mongoose").Page;
+const Admins = require("./config/mongoose").Admins;
+
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+const getYouTubeID = require("get-youtube-id");
+const fetchVideoInfo = require("youtube-info");
+
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-const server = createServer(app);
-
-// Mongo
-
-mongoose
-  .connect(
-    "mongodb+srv://redalert:redalert@cluster0-vzasu.mongodb.net/promo-csmoney?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-const AdminSchema = new mongoose.Schema({
-  login: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+app.get("/", (req, res) => {
+  res.sendFile(123);
 });
-
-// pagesSchema
-
-const PageSchema = new mongoose.Schema({
-  title: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  alias: {
-    type: String,
-  },
-  headerLogo: {
-    type: String,
-  },
-  chatLogo: {
-    type: String,
-  },
-  bloggerName: {
-    type: String,
-  },
-  mainHeader: {
-    type: String,
-  },
-  underTitileText: {
-    type: String,
-  },
-  buttonText: {
-    type: String,
-  },
-  infoTitle: {
-    type: String,
-  },
-  firstImageText: {
-    type: String,
-  },
-  secondImageText: {
-    type: String,
-  },
-  thirdImageText: {
-    type: String,
-  },
-  fourthImageText: {
-    type: String,
-  },
-  feedbackHeader: {
-    type: String,
-  },
-  feedbackText: {
-    type: String,
-  },
-  reviewLink: {
-    type: String,
-  },
-  csmoneyAnswer: {
-    type: String,
-  },
-  firsVideoLink: {
-    type: String,
-  },
-  secondVideoLink: {
-    type: String,
-  },
-  thirdVideoLink: {
-    type: String,
-  },
-  useCsmoneyLink: {
-    type: String,
-  },
-  finalBlockHeader: {
-    type: String,
-  },
-  finalBlockText: {
-    type: String,
-  },
-  promocode: {
-    type: String,
-  },
-  finalButtonText: {
-    type: String,
-  },
-  textUnderFinalButton: {
-    type: String,
-  },
-});
-
-const Admins = mongoose.model("admins", AdminSchema);
-const Page = mongoose.model("pages", PageSchema);
-
-// End mongo
 
 app.get("/shimbabumba", (req, res) => {
   res.render(__dirname + "/views/login.ejs");
@@ -141,44 +35,62 @@ app.post("/shimbabumba", urlencodedParser, (req, res) => {
   }
 });
 
-app.get("/shimbabumba/admin", (req, res) => {
-  Page.find({}, { title: 1 }).then((result) => {
-    console.log("result:", result);
-    res.render(__dirname + "/views/shimbabumba.ejs", {
-      titles: result,
-    });
+app.get("/shimbabumba/admin", async (req, res) => {
+  const result = await Page.find({}, { title_en: 1 });
+  res.render(__dirname + "/views/shimbabumba.ejs", {
+    pages: result,
   });
 });
 app.post("/shimbabumba/admin", urlencodedParser, (req, res) => {
   if (!req.body) return res.sendStatus(400);
-  console.log(req.body);
   Page.create({
-    title: req.body.title,
-    description: req.body.description,
-    alias: req.body.alias,
-    headerLogo: req.body.firstLogo,
-    chatLogo: req.body.secondLogo,
-    bloggerName: req.body.bloggerName,
-    mainHeader: req.body.mainHeader,
-    underTitileText: req.body.subtitle,
-    buttonText: req.body.buttonText,
-    infoTitle: req.body.infoHeader,
-    firstImageText: req.body.imageText1,
-    secondImageText: req.body.imageText2,
-    thirdImageText: req.body.imageText3,
-    fourthImageText: req.body.imageText4,
-    feedbackHeader: req.body.feedbackHeader,
-    feedbackText: req.body.feedbackText,
-    reviewLink: req.body.reviewLink,
-    csmoneyAnswer: req.body.csmoneyAnswer,
-    firsVideoLink: req.body.video1,
-    secondVideoLink: req.body.video1,
-    thirdVideoLink: req.body.video3,
-    useCsmoneyLink: req.body.useCsmoneywith,
-    finalBlockHeader: req.body.finalHeader,
+    title_en: req.body.title_en,
+    description_en: req.body.description_en,
+    alias_en: req.body.alias_en,
+    headerLogo: req.body.headerLogo,
+    chatLogo: req.body.chatLogo,
+    bloggerName_en: req.body.bloggerName_en,
+    mainHeader_en: req.body.mainHeader_en,
+    underTitleText_en: req.body.underTitleText_en,
+    buttonText_en: req.body.buttonText_en,
+    infoHeader_en: req.body.infoHeader_en,
+    firstImageText_en: req.body.firstImageText_en,
+    secondImageText_en: req.body.secondImageText_en,
+    thirdImageText_en: req.body.thirdImageText_en,
+    fourthImageText_en: req.body.fourthImageText_en,
+    feedbackHeader_en: req.body.feedbackHeader_en,
+    feedbackText_en: req.body.feedbackText_en,
+    reviewLink_en: req.body.reviewLink_en,
+    csmoneyAnswer_en: req.body.csmoneyAnswer_en,
+    firstVideoLink: req.body.firstVideoLink,
+    secondVideoLink: req.body.secondVideoLink,
+    thirdVideoLink: req.body.thirdVideoLink,
+    useCsmoneyTitle_en: req.body.useCsmoneyTitle_en,
+    finalBlockHeader_en: req.body.finalBlockHeader_en,
     promocode: req.body.promocode,
-    finalButtonText: req.body.finalButton,
-    textUnderFinalButton: req.body.textUnderFinalButton,
+    finalButtonText_en: req.body.finalButtonText_en,
+    textUnderFinalButton_en: req.body.textUnderFinalButton_en,
+
+    title_ru: req.body.title_ru,
+    description_ru: req.body.description_ru,
+    alias_ru: req.body.alias_ru,
+    bloggerName_ru: req.body.bloggerName_ru,
+    mainHeader_ru: req.body.mainHeader_ru,
+    underTitleText_ru: req.body.underTitleText_ru,
+    buttonText_ru: req.body.buttonText_ru,
+    infoHeader_ru: req.body.infoHeader_ru,
+    firstImageText_ru: req.body.firstImageText_ru,
+    secondImageText_ru: req.body.secondImageText_ru,
+    thirdImageText_ru: req.body.thirdImageText_ru,
+    fourthImageText_ru: req.body.fourthImageText_ru,
+    feedbackHeader_ru: req.body.feedbackHeader_ru,
+    feedbackText_ru: req.body.feedbackText_ru,
+    reviewLink_ru: req.body.reviewLink_ru,
+    csmoneyAnswer_ru: req.body.csmoneyAnswer_ru,
+    useCsmoneyTitle_ru: req.body.useCsmoneyTitle_ru,
+    finalBlockHeader_ru: req.body.finalBlockHeader_ru,
+    finalButtonText_ru: req.body.finalButtonText_ru,
+    textUnderFinalButton_ru: req.body.textUnderFinalButton_ru,
   }).then(res.redirect("/shimbabumba/admin"));
 });
 
@@ -186,25 +98,115 @@ app.post("/shimbabumba/admin", urlencodedParser, (req, res) => {
 //   res.status(404).send("The page doesn't exist");
 // });
 
-app.get("/:id", (req, res) => {
-  const id = req.params.id;
-  Page.findOne({ title: id }, function (err, user) {
-    if (err) return console.log(err);
-    console.log(user);
-    res.render(__dirname + "/views/influencer.ejs", {
-      pageInfo: user,
-    });
-  });
+// app.get("/:id", (req, res) => {
+//   const id = req.params.id;
+//   Page.findOne({ title: id }, function (err, user) {
+//     if (err) return console.log(err);
+//     console.log(user);
+//     res.render(__dirname + "/views/influencer.ejs", {
+//       pageInfo: user,
+//     });
+//   });
+// });
 
-  // Page.find({ title: req.params.id }).then((result) => {
-  //   console.log(result);
-  //   res.render(__dirname + "/views/influencer.ejs", {
-  //     pageInfo: result,
-  //   });
-  // });
+app.get("/influencer/:name", async (req, res) => {
+  try {
+    const result = await Page.find({ title_en: req.params.name });
+    const firstVideoID = getYouTubeID(result[0].firstVideoLink);
+    const secondVideoID = getYouTubeID(result[0].secondVideoLink);
+    const thirdVideoID = getYouTubeID(result[0].thirdVideoLink);
+    const firstVideoInfo = await fetchVideoInfo(firstVideoID);
+    const secondVideoInfo = await fetchVideoInfo(secondVideoID);
+    const thirdVideoInfo = await fetchVideoInfo(thirdVideoID);
+    res.render(__dirname + "/views/influencer.ejs", {
+      pageInfo: result[0],
+      firstVideoTitle: firstVideoInfo.title,
+      secondVideoTitle: secondVideoInfo.title,
+      thirdVideoTitle: thirdVideoInfo.title,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
-server.listen(port, () => console.log(`server is up. port : ${port}`));
-/* 
-Сделать, чтобы отображение страницы инфлюенсера было нормальным.
-сделать опцию редактирования страницы.
+
+app.get("/influencer/edit/:name", async (req, res) => {
+  const result = await Page.find({ title_en: req.params.name });
+  res.render("edit.ejs", {
+    user: result[0],
+  });
+});
+app.post("/influencer/edit/:name", urlencodedParser, (req, res) => {
+  Page.findOneAndUpdate(
+    { title_en: req.body.title_en },
+    {
+      title_en: req.body.title_en,
+      description_en: req.body.description_en,
+      alias_en: req.body.alias_en,
+      headerLogo: req.body.headerLogo,
+      chatLogo: req.body.chatLogo,
+      bloggerName_en: req.body.bloggerName_en,
+      mainHeader_en: req.body.mainHeader_en,
+      underTitleText_en: req.body.underTitleText_en,
+      buttonText_en: req.body.buttonText_en,
+      infoHeader_en: req.body.infoHeader_en,
+      firstImageText_en: req.body.firstImageText_en,
+      secondImageText_en: req.body.secondImageText_en,
+      thirdImageText_en: req.body.thirdImageText_en,
+      fourthImageText_en: req.body.fourthImageText_en,
+      feedbackHeader_en: req.body.feedbackHeader_en,
+      feedbackText_en: req.body.feedbackText_en,
+      reviewLink_en: req.body.reviewLink_en,
+      csmoneyAnswer_en: req.body.csmoneyAnswer_en,
+      firstVideoLink: req.body.firstVideoLink,
+      secondVideoLink: req.body.secondVideoLink,
+      thirdVideoLink: req.body.thirdVideoLink,
+      useCsmoneyTitle_en: req.body.useCsmoneyTitle_en,
+      finalBlockHeader_en: req.body.finalBlockHeader_en,
+      promocode: req.body.promocode,
+      finalButtonText_en: req.body.finalButtonText_en,
+      textUnderFinalButton_en: req.body.textUnderFinalButton_en,
+
+      title_ru: req.body.title_ru,
+      description_ru: req.body.description_ru,
+      alias_ru: req.body.alias_ru,
+      bloggerName_ru: req.body.bloggerName_ru,
+      mainHeader_ru: req.body.mainHeader_ru,
+      underTitleText_ru: req.body.underTitleText_ru,
+      buttonText_ru: req.body.buttonText_ru,
+      infoHeader_ru: req.body.infoHeader_ru,
+      firstImageText_ru: req.body.firstImageText_ru,
+      secondImageText_ru: req.body.secondImageText_ru,
+      thirdImageText_ru: req.body.thirdImageText_ru,
+      fourthImageText_ru: req.body.fourthImageText_ru,
+      feedbackHeader_ru: req.body.feedbackHeader_ru,
+      feedbackText_ru: req.body.feedbackText_ru,
+      reviewLink_ru: req.body.reviewLink_ru,
+      csmoneyAnswer_ru: req.body.csmoneyAnswer_ru,
+      useCsmoneyTitle_ru: req.body.useCsmoneyTitle_ru,
+      finalBlockHeader_ru: req.body.finalBlockHeader_ru,
+      finalButtonText_ru: req.body.finalButtonText_ru,
+      textUnderFinalButton_ru: req.body.textUnderFinalButton_ru,
+    }
+  );
+  res.redirect("/shimbabumba/admin");
+});
+
+app.get("/influencer/delete/:name", (req, res) => {
+  console.log("get params", req.params);
+  Page.findOneAndDelete({ title_en: req.params.name }).then((err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/shimbabumba/admin");
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on port 3000");
+});
+
+/*
+Сделать сессии
+Поставить регулярки по ТЗ
+Раскидать код по файлам
 */
